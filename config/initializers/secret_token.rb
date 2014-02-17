8f3375155a06b81b2c0a9f04d3769875d2a3d3a8
Sam Paul <sam@docurated.com>
2014-02-17 18:51:21 -0500

    readded secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SCrape::Application.config.secret_key_base = 'eb19bae651234e6215c93625986eb782db311b5358fd847e8062e26996e60148174524bf5a2d5dcc8bf85b26c5d37f2efaa1d9697a945f9b1c6b91c8c41bbf31'
